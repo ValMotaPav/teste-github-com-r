b4fcb3e1b83c4f9e3e8abbff08945a691c59720a
Valentina Mota Pavelecini <valentina.mota.pav@gmail.com>
2026-02-24 12:09:32 -0300

    Graficos pokemon e afins

--- teste pokemon.R
+++ teste pokemon.R
@@ -4,5 +4,52 @@ library(pokemon)
 view(pokemon)
 
 pokemon %>% 
-  ggplot(aes(x = attack)) + geom_dotplot(binwidth = 1)
+  ggplot() + 
+  geom_jitter(aes(defense, attack, color=type_1)) +
+  scale_color_manual(values = c("dragon" = "darkslategrey",
+                                "dark" = "black",
+                                "bug" = "darkolivegreen",
+                                "electric" = "yellow",
+                                "fairy" = "pink",
+                                "fighting" = "chocolate",
+                                "fire" = "red",
+                                "flying" = "cornflowerblue",
+                                "ghost" = "darkslateblue",
+                                "grass" = "green",
+                                "ground" = "brown4",
+                                "ice" = "darkslategray3",
+                                "normal" = "azure4",
+                                "poison" = "darkorchid",
+                                "psychic" = "deeppink4",
+                                "rock" = "orange",
+                                "steel" = "cornsilk2",
+                                "water" = "deepskyblue3")) +
+  scale_y_continuous(breaks = seq(0, 200, by = 10)) +
+  scale_x_continuous(breaks = seq(0, 240, by = 10))
+
+pokemon %>% 
+  ggplot() + 
+  geom_jitter(aes(type_1, defense, color = type_1), width = 0.2) +
+  scale_y_continuous(breaks = seq(0, 240, by = 10)) + 
+  scale_color_manual(values = c("dragon" = "darkslategrey",
+                                "dark" = "black",
+                                "bug" = "darkolivegreen",
+                                "electric" = "yellow",
+                                "fairy" = "pink",
+                                "fighting" = "chocolate",
+                                "fire" = "red",
+                                "flying" = "cornflowerblue",
+                                "ghost" = "darkslateblue",
+                                "grass" = "green",
+                                "ground" = "brown4",
+                                "ice" = "darkslategray3",
+                                "normal" = "azure4",
+                                "poison" = "darkorchid",
+                                "psychic" = "deeppink4",
+                                "rock" = "orange",
+                                "steel" = "cornsilk2",
+                                "water" = "deepskyblue3"))
+
+filter(pokemon, attack >= 180)
+filter(pokemon, defense >= 200)
 
